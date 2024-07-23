3a8c03ad7a54fdbcb19a9b093107496503a42927
vishnuvardhan <134271319+vishnuvardhan2114@users.noreply.github.com>
2024-07-24 00:11:29 +0530

    create user change and server change

--- client/src/elements/Create.jsx
+++ client/src/elements/Create.jsx
@@ -1,137 +1,155 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 function Create() {
-    const [values, setValues] = useState({
-        firstname: '',
-        lastname: '',
-        location: '',
-        email: '',
-        dob: '',
-        education: '',
-        about: '',
+  const [values, setValues] = useState({
+    first_name: "",
+    last_name: "",
+    location: "",
+    email: "",
+    dob: "",
+    education: "",
+    about: "",
+  });
+
+  const navigate = useNavigate();
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
     });
+  }
 
-    const navigate = useNavigate();
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8081/students", values)
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-
-        axios.post('/add_user', values)
-            .then((res) => {
-                navigate('/');
-                console.log(res);
-            })
-            .catch((err) => console.log(err));
-    }
-
-    function handleInputChange(e) {
-        const { name, value } = e.target;
-        setValues({
-            ...values,
-            [name]: value,
-        });
-    }
-
-    return (
-        <div className="container">
-            <div className="row">
-                <h3 className="text-center display-4 my-4">Update Student Details</h3>
-                <div className="d-flex justify-content-end my-3">
-                    <Link to="/" className="btn btn-dark">Home</Link>
-                </div>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <label htmlFor="firstname" className="form-label">First Name</label>
-                        <input
-                            type="text"
-                            name="firstname"
-                            value={values.firstname}
-                            required
-                            onChange={handleInputChange}
-                            className="form-control"
-                            style={{ maxWidth: '300px' }}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="lastname" className="form-label">Last Name</label>
-                        <input
-                            type="text"
-                            name="lastname"
-                            value={values.lastname}
-                            required
-                            onChange={handleInputChange}
-                            className="form-control"
-                            style={{ maxWidth: '300px' }}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="location" className="form-label">Location</label>
-                        <input
-                            type="text"
-                            name="location"
-                            value={values.location}
-                            required
-                            onChange={handleInputChange}
-                            className="form-control"
-                            style={{ maxWidth: '300px' }}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email</label>
-                        <input
-                            type="email"
-                            name="email"
-                            value={values.email}
-                            required
-                            onChange={handleInputChange}
-                            className="form-control"
-                            style={{ maxWidth: '300px' }}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="dob" className="form-label">DOB</label>
-                        <input
-                            type="date"
-                            name="dob"
-                            value={values.dob}
-                            required
-                            onChange={handleInputChange}
-                            className="form-control"
-                            style={{ maxWidth: '300px' }}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="education" className="form-label">Education</label>
-                        <input
-                            type="text"
-                            name="education"
-                            value={values.education}
-                            required
-                            onChange={handleInputChange}
-                            className="form-control"
-                            style={{ maxWidth: '300px' }}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="about" className="form-label">About</label>
-                        <textarea
-                            name="about"
-                            value={values.about}
-                            onChange={handleInputChange}
-                            className="form-control"
-                            rows="4"
-                            style={{ maxWidth: '500px' }}
-                        ></textarea>
-                    </div>
-                    <div className="d-flex justify-content-center">
-                        <button type="submit" className="btn btn-dark mx-3">Save</button>
-                    </div>
-                </form>
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <h3 className="text-center display-4 my-4">Create Student Details</h3>
+        <div className="d-flex justify-content-end my-3">
+          <Link to="/" className="btn btn-dark">
+            Home
+          </Link>
         </div>
-    );
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="first_name" className="form-label">
+              First Name
+            </label>
+            <input
+              type="text"
+              name="first_name"
+              value={values.first_name}
+              required
+              onChange={handleInputChange}
+              className="form-control"
+              style={{ maxWidth: "300px" }}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="last_name" className="form-label">
+              Last Name
+            </label>
+            <input
+              type="text"
+              name="last_name"
+              value={values.last_name}
+              required
+              onChange={handleInputChange}
+              className="form-control"
+              style={{ maxWidth: "300px" }}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="location" className="form-label">
+              Location
+            </label>
+            <input
+              type="text"
+              name="location"
+              value={values.location}
+              required
+              onChange={handleInputChange}
+              className="form-control"
+              style={{ maxWidth: "300px" }}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={values.email}
+              required
+              onChange={handleInputChange}
+              className="form-control"
+              style={{ maxWidth: "300px" }}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="dob" className="form-label">
+              DOB
+            </label>
+            <input
+              type="date"
+              name="dob"
+              value={values.dob}
+              required
+              onChange={handleInputChange}
+              className="form-control"
+              style={{ maxWidth: "300px" }}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="education" className="form-label">
+              Education
+            </label>
+            <input
+              type="text"
+              name="education"
+              value={values.education}
+              required
+              onChange={handleInputChange}
+              className="form-control"
+              style={{ maxWidth: "300px" }}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="about" className="form-label">
+              About
+            </label>
+            <textarea
+              name="about"
+              value={values.about}
+              onChange={handleInputChange}
+              className="form-control"
+              rows="4"
+              style={{ maxWidth: "500px" }}
+            ></textarea>
+          </div>
+          <div className="d-flex justify-content-center">
+            <button type="submit" className="btn btn-dark mx-3">
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Create;
